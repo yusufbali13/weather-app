d152fb3c1200113025940857e62c8b6e2e4f051a
Yusuf <baliyusuf675@gmail.com>
2023-06-20 18:31:18 +0300

    icon

--- app.js
+++ app.js
@@ -26,14 +26,14 @@ const wheatherTable = (data) => {
     name,
     sys: { country },
     main: { temp, feels_like, humidity },
-    weather: [{ main, description }],
+    weather: [{ main, description, icon }],
     wind: { speed },
   } = data;
   const container = document.querySelector(".container");
   container.innerHTML = `
   <div class="card p-4 " style="width: 18rem;">
   <!--* Resim Alanı -->
-  <img src="" class="card-img-top" alt="...">
+  <img src="" class="card-img-top mx-auto icon-img"  alt="...">
   <!--* Şehir-Ulke-Alani -->
   <div class="card-body">
     <h2 class="card-title " id="şehir-adi">${name} </h2><span class="ülke" id="ülke">${country}</span>
@@ -57,7 +57,7 @@ const wheatherTable = (data) => {
   </ul>
   <div class="card-body d-flex justify-content-between border  p-4 ">
   <div class="rüzgar-hizi d-flex align-items-center gap-1 ">
-  <img src="./nem.jpg" alt="" width="40%" class="mt-4">
+  <img src="./rüzgar.png" alt="" width="40%" class="mt-4">
                <div class=" mt-4">
                      <div class="d-flex align-items-center">
                           <span class="wind h1" id="rüzgar-hizi">${speed} </span> <strong class="h3 mx-2">km/h</strong>
@@ -65,7 +65,7 @@ const wheatherTable = (data) => {
                </div>
               </div>
   <div class="nem-durumu d-flex align-items-center mx-5  gap-1 ">
-        <img src="./rüzgar.png" alt="" width="50%" class="mt-4">
+        <img src="./nem.jpg" alt="" width="50%" class="mt-4">
         <div class="d-flex mt-4">
               <span class="wind h1" id="nem-orani">${humidity}</span> <span class="h1">%</span>
         </div>
